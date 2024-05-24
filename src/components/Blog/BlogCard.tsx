8869bc0fd0719a8008ffce30d09976b3a52cc8f7
CocaIsMee <anhphukareno@hphukarenogmail.arenogmailcom>
2024-05-24 17:58:08 +0700

    update searchtool

--- src/components/Blog/BlogCard.tsx
+++ src/components/Blog/BlogCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, Row, Col } from 'antd';
+import React, { useState } from 'react';
+import { Card, Row, Col, Input } from 'antd';
 import { Link } from 'react-router-dom';
 import { Blog } from './Blog'; // Import the Blog interface
 
@@ -8,23 +8,38 @@ interface Props {
 }
 
 const BlogCard: React.FC<Props> = ({ blogs }) => {
+    const [searchTerm, setSearchTerm] = useState<string>('');
+
+    // Filter blogs based on search term
+    const filteredBlogs = blogs.filter(blog =>
+        blog.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <Row gutter={[16, 16]}>
-            {blogs.map(blog => (
-                <Col key={blog.id} xs={24} sm={12} md={8} lg={6}> {/* Each card takes 6 columns on lg screens, 8 columns on md screens, 12 columns on sm screens, and 24 columns on xs screens */}
-                    <Card
-                        title={blog.title}
-                        extra={<Link to={`/blogs/${blog.id}`}>More</Link>}
-                        hoverable
-                        style={{ width: '100%' }}
-                        cover={<img src={blog.image} alt={blog.title} style={{ width: '100%' }} />}
-                    >
-                        <p>{blog.description}</p>
-                        <p>Status: {blog.status}</p>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+        <div>
+            <Input
+                placeholder="Search by title"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                style={{ marginBottom: '16px' }}
+            />
+            <Row gutter={[16, 16]}>
+                {filteredBlogs.map(blog => (
+                    <Col key={blog.id} xs={24} sm={12} md={8} lg={6}>
+                        <Card
+                            title={blog.title}
+                            extra={<Link to={`/blogs/${blog.id}`}>More</Link>}
+                            hoverable
+                            style={{ width: '100%' }}
+                            cover={<img src={blog.image} alt={blog.title} style={{ width: '100%' }} />}
+                        >
+                            <p>{blog.description}</p>
+                            <p>Status: {blog.status}</p>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </div>
     );
 };
 
